c117e922f6e133cd1b3657547afc624d925367da
lalyos <lalyos@yahoo.com>
2015-02-02 17:59:46 +0100

    unifying provisioner script for all cloud provider
    public_hostname and disk creation scripts are choosen by the PACKER_BUILDER_TYPE
    which is automaticaaly passed: https://www.packer.io/docs/provisioners/shell.html#PACKER_BUILDER_TYPE

--- user-data-script.sh
+++ user-data-script.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# let the script fail to cause the image build unsuccessfull
+# set -e
+
 [[ "$TRACE" ]] && set -x
 
 : ${IMAGES:=sequenceiq/ambari:1.7.0-consul sequenceiq/consul:v0.4.1.ptr}
+: ${DEBUG:=1}
+
+debug() {
+    [[ "$DEBUG" ]] && echo "-----> $*" 1>&2
+}
 
 install_utils() {
   apt-get update && apt-get install -y unzip curl git python-pip dnsutils nmap
@@ -28,12 +36,36 @@ pull_images() {
   done
 }
 
+github_raw_content() {
+
+    local output=${1:? required as 1. parameter}
+    local path=${2:? reuired as 2. parmeter}
+
+    local repo=sequenceiq/cloudbreak-images
+
+    debug "download raw $repo/$path => $output"
+    curl \
+        -o ${output} \
+        -H "Accept: application/vnd.github.VERSION.raw" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        https://api.github.com/repos/${repo}/contents/${path}
+}
+
 install_scripts() {
-  curl -o /usr/local/register-ambari.sh https://gist.githubusercontent.com/doktoric/bbc20ec450573697c348/raw/register-ambari && chmod +x /usr/local/register-ambari.sh
-  curl -o /usr/local/public_host_script.sh https://gist.githubusercontent.com/doktoric/41a28da9e974029d95d2/raw/ec2-host && chmod +x /usr/local/public_host_script.sh
-  curl -o /usr/local/disk_mount.sh https://gist.githubusercontent.com/doktoric/0277e537cd97b0d9762f/raw/ec2-disk && chmod +x /usr/local/disk_mount.sh
-  cp /usr/local/register-ambari.sh /etc/init.d/register-ambari
-  chmod +x /etc/init.d/register-ambari
+  local target=${1:-/usr/local}
+  local provider=$(get_provider_from_packer)
+
+  debug target=$target
+  debug provider=$provider
+  
+  github_raw_content ${target}/register-ambari.sh scripts/register-ambari.sh
+  github_raw_content ${target}/public_host_script.sh scripts/public_host_script_${provider}.sh
+  github_raw_content ${target}/disk_mount.sh scripts/disk_mount_${provider}.sh?
+  
+  chmod +x ${target}/*.sh
+  ls -l $target
+  
+  cp ${target}/register-ambari.sh /etc/init.d/register-ambari
   chown root:root /etc/init.d/register-ambari
   update-rc.d -f register-ambari defaults
   update-rc.d -f register-ambari enable
@@ -43,7 +75,34 @@ fix_fstab() {
 	sed -i "/dev\/xvdb/ d" /etc/fstab
 }
 
+get_provider_from_packer() {
+    : ${PACKER_BUILDER_TYPE:? required amazon-ebs/googlecompute/openstack}
+    
+    if [[ $PACKER_BUILDER_TYPE =~ amazon ]] ; then 
+        echo ec2
+        return
+    fi
+
+    if [[ $PACKER_BUILDER_TYPE == "googlecompute" ]]; then
+        echo gce
+        return
+    fi
+
+    if [[ $PACKER_BUILDER_TYPE == "openstack" ]]; then
+        echo openstack
+        return
+    fi
+
+    echo UNKNOWN_PROVIDER
+}
+
+check_params() {
+    : ${GITHUB_TOKEN:? required}
+    : ${PACKER_BUILDER_TYPE:? required amazon-ebs/googlecompute/openstack }
+}
+
 main() {
+    check_params
     install_utils
     install_docker
     pull_images
