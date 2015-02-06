f1e27588f7002e78454cde5399509cab0b0949a6
lalyos <lalyos@yahoo.com>
2015-02-06 19:05:28 +0100

    set -e only for imae pulls

--- user-data-script.sh
+++ user-data-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # let the script fail to cause the image build unsuccessfull
-set -eo pipefail
+# set -eo pipefail
 
 [[ "$TRACE" ]] && set -x
 
@@ -29,6 +29,7 @@ install_consul() {
 }
 
 pull_images() {
+  set -e
   for i in ${IMAGES}; do
     docker pull ${i}
   done
