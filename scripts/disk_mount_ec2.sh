8a7976a4fffe5f11a5171aa56627a62d7ea78f65
Ferenc Schneider <ferenc.schneider@sequenceiq.com>
2015-03-26 15:32:31 +0100

    CLOUD-546 fix: mounting attached disks under hadoopfs dir instead of mnt

--- scripts/disk_mount_ec2.sh
+++ scripts/disk_mount_ec2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 START_LABEL=65
+mkdir /hadoopfs
 for (( i=1; i<=15; i++ )); do
   LABEL=$(printf "\x$((START_LABEL+i))")
   if [ -e /dev/xvd"$LABEL" ]; then
     mkfs -t ext4 /dev/xvd${LABEL}
-    mkdir /mnt/fs${i}
-    echo /dev/xvd${LABEL} /mnt/fs${i} ext4  defaults 0 2 >> /etc/fstab
-    mount /mnt/fs${i}
-    DOCKER_VOLUME_PARAMS="${DOCKER_VOLUME_PARAMS} -v /mnt/fs${i}:/mnt/fs${i}"
+    mkdir /hadoopfs/fs${i}
+    echo /dev/xvd${LABEL} /hadoopfs/fs${i} ext4  defaults 0 2 >> /etc/fstab
+    mount /hadoopfs/fs${i}
+    DOCKER_VOLUME_PARAMS="${DOCKER_VOLUME_PARAMS} -v /hadoopfs/fs${i}:/hadoopfs/fs${i}"
   fi
 done
--- scripts/disk_mount_openstack.sh
+++ scripts/disk_mount_openstack.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 START_LABEL=61
+mkdir /hadoopfs
 for (( i=1; i<=15; i++ )); do
   LABEL=$(printf "\x$((START_LABEL+i))")
   if [ -e /dev/vd"$LABEL" ]; then
     mkfs -t ext4 /dev/vd${LABEL}
-    mkdir /mnt/fs${i}
-    echo /dev/vd${LABEL} /mnt/fs${i} ext4  defaults 0 2 >> /etc/fstab
-    mount /mnt/fs${i}
-    DOCKER_VOLUME_PARAMS="${DOCKER_VOLUME_PARAMS} -v /mnt/fs${i}:/mnt/fs${i}"
+    mkdir /hadoopfs/fs${i}
+    echo /dev/vd${LABEL} /hadoopfs/fs${i} ext4  defaults 0 2 >> /etc/fstab
+    mount /hadoopfs/fs${i}
+    DOCKER_VOLUME_PARAMS="${DOCKER_VOLUME_PARAMS} -v /hadoopfs/fs${i}:/hadoopfs/fs${i}"
   fi
 done
