3c4f0dd3583fc83be4826c970a45195155c0d311
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2015-04-20 12:59:27 +0200

    update terraform to use 30 GB disk

--- google.tf
+++ google.tf
@@ -43,7 +43,7 @@ resource "google_compute_disk" "image-builder-disk" {
     name = "image-builder-disk-${var.gcc_identify}"
     type = "pd-ssd"
     zone = "${var.gce_zone}"
-    size = "30"
+    size = "100"
     #image = "debian7-wheezy"
 }
 
