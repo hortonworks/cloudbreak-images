572921496746296f9ca7b8e49c3fccb414b1de2e
Richard Doktorics <rdoktorics@hortonworks.com>
2015-06-30 13:42:43 +0200

    CLOUD-859 removed sequenceiq from tar.gz name because the name of the storage already contains it

--- create_gc_image.sh
+++ create_gc_image.sh
@@ -6,4 +6,4 @@ sudo gcimagebundle -d /dev/sda --skip_disk_space_check -o /mnt/tmp/ --log_file=/
 curl -O https://storage.googleapis.com/pub/gsutil.tar.gz
 tar xfz gsutil.tar.gz -C $HOME
 export PATH=${PATH}:$HOME/gsutil
-gsutil cp /mnt/tmp/*.image.tar.gz gs://sequenceiqimage/sequenceiq-ambari17-consul-$(date +%Y-%m-%d-%H%M).image.tar.gz
+gsutil cp /mnt/tmp/*.image.tar.gz gs://sequenceiqimage/ambari17-consul-$(date +%Y-%m-%d-%H%M).image.tar.gz
