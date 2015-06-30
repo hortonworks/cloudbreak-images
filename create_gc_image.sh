9cf1bbbceeca378930260506d6526f947b1f17fc
Richard Doktorics <rdoktorics@hortonworks.com>
2015-06-30 12:55:03 +0200

    CLOUD-859 fixed google cloud image creator script

--- create_gc_image.sh
+++ create_gc_image.sh
@@ -2,5 +2,8 @@
 
 sudo mkdir /mnt/tmp
 sudo /usr/share/google/safe_format_and_mount -m "mkfs.ext4 -F" /dev/sdb /mnt/tmp
-sudo gcimagebundle -d /dev/sda -o /mnt/tmp/ --log_file=/tmp/abc.log
+sudo gcimagebundle -d /dev/sda --skip_disk_space_check -o /mnt/tmp/ --log_file=/tmp/abc.log
+curl -O https://storage.googleapis.com/pub/gsutil.tar.gz
+tar xfz gsutil.tar.gz -C $HOME
+export PATH=${PATH}:$HOME/gsutil
 gsutil cp /mnt/tmp/*.image.tar.gz gs://sequenceiqimage/sequenceiq-ambari17-consul-$(date +%Y-%m-%d-%H%M).image.tar.gz
