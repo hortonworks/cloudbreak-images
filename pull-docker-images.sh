6a60019a638ff568b029eaceac94dd73fe83ed1d
lalyos <lalyos@yahoo.com>
2016-01-29 15:38:22 +0100

    extend-root-fs, modify-waagent and configure_cloud_init moved out from base image

--- pull-docker-images.sh
+++ pull-docker-images.sh
@@ -52,9 +52,43 @@ reset_docker() {
   rm -vf /etc/docker/key.json
 }
 
+extend_rootfs() {
+  yum -y install cloud-utils-growpart
+
+  # Usable on GCP, does not harm anywhere else
+  root_fs_device=$(mount | grep ' / ' | cut -d' ' -f 1 | sed s/1//g)
+  growpart $root_fs_device 1 || :
+  xfs_growfs / || :
+}
+
+configure_cloud_init() {
+  if [ -f /etc/cloud/cloud.cfg ]; then
+    #/etc/sysconfig/network is not used by CentOS 7 anymore
+    sed -i.bak '/syslog_fix_perms: ~/a preserve_hostname: true' /etc/cloud/cloud.cfg
+    diff /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.bak || :
+  fi
+}
+
+
+modify_waagent() {
+  if [ -f /etc/waagent.conf ]; then
+    cp /etc/waagent.conf /etc/waagent.conf.bak
+    sed -i 's/Provisioning.SshHostKeyPairType.*/Provisioning.SshHostKeyPairType=ecdsa/' /etc/waagent.conf
+    sed -i 's/Provisioning.DecodeCustomData.*/Provisioning.DecodeCustomData=y/' /etc/waagent.conf
+    sed -i 's/Provisioning.ExecuteCustomData.*/Provisioning.ExecuteCustomData=y/' /etc/waagent.conf
+    diff /etc/waagent.conf /etc/waagent.conf.bak || :
+
+    sed -i '/ExecStart=/ i ExecStartPre=/usr/bin/docker-helper' /etc/systemd/system/docker.service
+  fi
+}
+
+
 
 main() {
   init
+  extend_rootfs
+  configure_cloud_init
+  modify_waagent
   reinstall_docker
   start_docker
   docker_pull_images "$@"
