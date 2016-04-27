5c0435e5424978e269412c6ec9f40d846f507bc1
Ferenc Schneider <ferenc.schneider@sequenceiq.com>
2016-04-27 16:42:00 +0200

    CLOUD-56481: Fix of slow lazy format on azure

--- shared/usr/bin/user-data-helper.sh
+++ shared/usr/bin/user-data-helper.sh
@@ -6,6 +6,7 @@ set
 : ${CLOUD_PLATFORM:? required}
 : ${START_LABEL:? required}
 : ${PLATFORM_DISK_PREFIX:? required}
+: ${LAZY_FORMAT_DISK_LIMIT:? required}
 : ${IS_GATEWAY:? required}
 : ${TMP_SSH_KEY:? required}
 : ${PUBLIC_SSH_KEY:? required}
@@ -84,6 +85,56 @@ relocate_docker() {
 }
 
 format_disks() {
+  local disks=( $(list_attached_disks) )
+  if [[ $CLOUD_PLATFORM == AZURE* ]] && [ "${#disks[*]}" -gt "$LAZY_FORMAT_DISK_LIMIT" ]; then
+    format_disks_with_itable_init "${disks[@]}"
+    mount_disks "${disks[@]}"
+  else
+    lazy_format_disks
+  fi
+  cd /hadoopfs/fs1 && mkdir logs logs/ambari-server logs/ambari-agent logs/consul-watch logs/kerberos
+}
+
+list_attached_disks() {
+  local ind=0
+  local disks=()
+  for (( i=1; i<=24; i++ )); do
+    label=$(printf "\x$(printf %x $((START_LABEL+i)))")
+    device=/dev/${PLATFORM_DISK_PREFIX}${label}
+    if [ -e $device ]; then
+      disks[$ind]=$device
+      (( ind++ ))
+    fi
+  done
+  echo "${disks[*]}"
+}
+
+format_disks_with_itable_init() {
+  local disks=( "${@}" )
+  for disk in "${disks[@]}"; do
+    MOUNTPOINT=$(grep $disk /etc/fstab | tr -s ' \t' ' ' | cut -d' ' -f 2)
+    if [ -n "$MOUNTPOINT" ]; then
+      umount "$MOUNTPOINT"
+      sed -i "\|^$disk|d" /etc/fstab
+    fi
+    mkfs -E lazy_itable_init=0,lazy_journal_init=0 -O uninit_bg -F -t ext4 $disk &
+  done
+  wait
+}
+
+mount_disks() {
+  local disks=( "${@}" )
+  mkdir /hadoopfs
+  for (( i=0; i<$(( ${#disks[@]} )); i++ )); do
+    mountPoint="/hadoopfs/fs$(( i+1 ))"
+    mkdir $mountPoint
+    echo UUID=$(blkid -o value ${disks[i]} | head -1) $mountPoint ext4  defaults,noatime,nofail 0 2 >> /etc/fstab
+    mount $mountPoint
+    chmod 777 $mountPoint
+  done
+}
+
+lazy_format_disks() {
   mkdir /hadoopfs
   for (( i=1; i<=24; i++ )); do
     LABEL=$(printf "\x$(printf %x $((START_LABEL+i)))")
@@ -101,7 +152,6 @@ format_disks() {
       chmod 777 /hadoopfs/fs${i}
     fi
   done
-  cd /hadoopfs/fs1 && mkdir logs logs/ambari-server logs/ambari-agent logs/consul-watch logs/kerberos
 }
 
 reload_sysconf() {
