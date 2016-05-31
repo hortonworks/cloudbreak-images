d0346d527897db9048b13d27a4416bb9965753dd
lalyos <lalyos@yahoo.com>
2016-05-31 23:45:58 +0200

    fix bash autocomplete

--- cbd-init.sh
+++ cbd-init.sh
@@ -30,7 +30,7 @@ cbd_init() {
     cbd generate
     cbd pull-parallel
 
-    cat >> ~/.bashrc <<EOF
+    cat >> ~/.bashrc <<"EOF"
 eval "$(bash -c 'cd /var/lib/cloudbreak-deployment; cbd bash-complete')"
 EOF
     rm -rf certs *.yml *.log
