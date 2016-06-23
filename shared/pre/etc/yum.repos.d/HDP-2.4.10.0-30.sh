9ba022d805275b17fbc09b8cfda65d440351afe7
Attila Kanto <attila.kanto@gmail.com>
2016-06-23 09:15:31 +0200

    CLOUD-58099 Update Ambari to 2.4.0.0 - 683 and update HDP

---
+++ shared/pre/etc/yum.repos.d/HDP-2.4.10.0-30.sh
@@ -0,0 +1,7 @@
+export STACK=HDP
+export STACK_VERSION=2.4
+export OS_TYPE_R6=redhat6
+export OS_TYPE_R7=redhat7
+export REPO_ID=HDP-2.4
+export BASE_URL_R6=http://s3.amazonaws.com/dev.hortonworks.com/HDP/centos6/2.x/BUILDS/2.4.10.0-30
+export BASE_URL_R7=http://s3.amazonaws.com/dev.hortonworks.com/HDP/centos7/2.x/BUILDS/2.4.10.0-30
