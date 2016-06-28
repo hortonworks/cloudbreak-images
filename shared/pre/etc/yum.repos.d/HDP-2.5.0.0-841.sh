8e8a60527b5e886f5ef248965bf59081d9c676a2
Attila Kanto <attila.kanto@gmail.com>
2016-06-28 14:55:05 -0700

    CLOUD-58099 Update to stack 841

---
+++ shared/pre/etc/yum.repos.d/HDP-2.5.0.0-841.sh
@@ -0,0 +1,7 @@
+export STACK=HDP
+export STACK_VERSION=2.5
+export OS_TYPE_R6=redhat6
+export OS_TYPE_R7=redhat7
+export REPO_ID=HDP-2.5
+export BASE_URL_R6=http://s3.amazonaws.com/dev.hortonworks.com/HDP/centos6/2.x/BUILDS/2.5.0.0-841
+export BASE_URL_R7=http://s3.amazonaws.com/dev.hortonworks.com/HDP/centos7/2.x/BUILDS/2.5.0.0-841
