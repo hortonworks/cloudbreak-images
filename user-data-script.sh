fbc1c733607b059a24222dbd54a69e3d58727d68
Attila Kanto <attila.kanto@gmail.com>
2016-06-29 06:54:52 -0700

    Revert "CLOUD-61210	LLAP start fails on AmazonLinux, we need to mimic that this Amazon is a CentOS"
    This reverts commit b24f4ce669ef915616e8f2bf5b7f713662264590.

--- user-data-script.sh
+++ user-data-script.sh
@@ -333,13 +333,6 @@ tune_vm() {
   fi
 }
 
-slider_patch_on_amazon() {
-  # Slider will fail on Amazon, we need to mimic that we are on CentOS
-  if grep "Amazon Linux AMI" /etc/issue &> /dev/null; then
-    echo "CentOS Linux release 7.2.1511 (Core)" > /etc/redhat-release
-  fi
-}
-
 main() {
     check_params
     update_centos
@@ -363,7 +356,6 @@ main() {
     tune_vm
     disable_swap
     set_dirty_ratio
-    slider_patch_on_amazon
     cleanup
     create_gc_image
 }
