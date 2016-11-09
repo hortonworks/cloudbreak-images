540c46502162af072598f867a574c0fa35f1faa3
lalyos <lalyos@yahoo.com>
2016-11-09 10:01:49 +0100

    add ssm_agent to base image

--- prometheus.sh
+++ prometheus.sh
@@ -20,6 +20,12 @@ install_tools() {
     yum -y install curl
 }
 
+install_ssm_agent() {
+    cd /tmp
+    curl https://amazon-ssm-eu-west-1.s3.amazonaws.com/latest/linux_amd64/amazon-ssm-agent.rpm -o amazon-ssm-agent.rpm
+    yum install -y amazon-ssm-agent.rpm
+}
+
 install_prometheus_exporters() {
     curl -Lks https://github.com/prometheus/node_exporter/releases/download/0.12.0/node_exporter-0.12.0.linux-amd64.tar.gz | tar --strip-components=1 -xz -C ${INSTALL_DIR}
     curl -Lks https://github.com/deathowl/spot_expiry_collector/releases/download/1.0.1/spot_expiry_collector-1.0.1.linux-amd64.tar.gz | tar  --strip-components=1 -xz -C ${INSTALL_DIR}
@@ -35,6 +41,7 @@ main() {
 
     install_tools
     install_prometheus_exporters
+    install_ssm_agent
 }
 
 [[ "$0" == "$BASH_SOURCE" ]] && main "$@"
