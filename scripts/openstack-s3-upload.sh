4af57b453e5fea7cd68566b097faf9338394f2e6
Gabor Kozma <gabo@kozmagabor.com>
2017-10-18 18:12:07 +0200

    add multiple atlas resource path support

--- scripts/openstack-s3-upload.sh
+++ scripts/openstack-s3-upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -xe
 
-: ${S3_TARGET:=s3://public-repo-1.hortonworks.com/HDP/cloudbreak}
-: ${OS_IMAGE_ATLAS_VERSION:=latest}
 : ${ATLAS_PROJECT:=cloudbreak}
+: ${S3_TARGET:=s3://public-repo-1.hortonworks.com/HDP/$ATLAS_PROJECT}
+: ${OS_IMAGE_ATLAS_VERSION:=latest}
 OS_IMAGE_NAME=$(curl -sL https://atlas.hashicorp.com/api/v1/artifacts/hortonworks/$ATLAS_PROJECT/openstack.image/search?version=$OS_IMAGE_ATLAS_VERSION | jq '.versions[0].metadata.image_name' -r)
 OS_IMAGE_ID=$(glance image-list | sed -n "/$OS_IMAGE_NAME / s/| \([^ ]*\).*/\1/p")
 glance image-download --file $OS_IMAGE_NAME.img --progress $OS_IMAGE_ID
