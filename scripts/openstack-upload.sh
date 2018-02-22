91f0e06d5bf5c9a7ecea7a018f88d6fece9b125b
Mihaly Laszlo Molnar <mmolnar@hortonworks.com>
2018-02-22 11:50:55 +0100

    BUG-97073 Openstack kilo upload should be streamlined #3

--- scripts/openstack-upload.sh
+++ scripts/openstack-upload.sh
@@ -8,7 +8,6 @@ OS_IMAGE_ID=$(glance image-list | sed -n "/$OS_IMAGE_NAME / s/| \([^ ]*\).*/\1/p
 glance image-download --file $OS_IMAGE_NAME.img --progress $OS_IMAGE_ID
 aws s3 cp $OS_IMAGE_NAME.img $S3_TARGET --acl public-read
 if [[ $OS_KILO_AUTH_URL ]] && [[ $OS_KILO_PASSWORD ]] && [[ $OS_KILO_TENANT_NAME ]] && [[ $OS_KILO_USERNAME ]]; then
-    glance image-create --name "$OS_IMAGE_NAME" --file "$OS_IMAGE_NAME.img"  --disk-format qcow2 --container-format bare \
-        --os-username "$OS_KILO_USERNAME" --os-tenant-name "$OS_KILO_TENANT_NAME" --os-auth-url "$OS_KILO_AUTH_URL" --os-password "$OS_KILO_PASSWORD"
+    glance image-create --name "$OS_IMAGE_NAME" --file "$OS_IMAGE_NAME.img"  --disk-format qcow2 --container-format bare --os-username "$OS_KILO_USERNAME" --os-tenant-name "$OS_KILO_TENANT_NAME" --os-auth-url "$OS_KILO_AUTH_URL" --os-password "$OS_KILO_PASSWORD"
 fi
 rm -f $OS_IMAGE_NAME.img
