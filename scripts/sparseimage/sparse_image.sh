713ab540aa2249fee1d9711103e302b62dac7dca
Peter Darvasi <pdarvasi@hortonworks.com>
2019-07-04 18:38:16 +0200

    CB-879 - Support Centos7 on AWS - minor corrections

--- scripts/sparseimage/sparse_image.sh
+++ scripts/sparseimage/sparse_image.sh
@@ -8,15 +8,7 @@ else
     sudo apt-get install -y ddpt
 fi
 
-sudo sfdisk /dev/xvdd <<EOF
-;
-;
-;
-;
-EOF
-sudo mkfs.xfs /dev/xvdd1
     sudo mkdir /image
-    sudo mount -t xfs /dev/xvdd1 /image
     sudo dd if=/dev/xvdb of=/image/sparse.img bs=1M status=progress
     sudo mkdir /loop
     sudo mount -t xfs -o loop,discard,offset=1048576 -o nouuid /image/sparse.img /loop
