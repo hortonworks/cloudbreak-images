eed4d22606007f1c6a9fad0a68cddf0c3e47c0de
Peter Darvasi <pdarvasi@hortonworks.com>
2019-07-15 21:37:41 +0200

    CB-879 - Support Centos7 on AWS - add jq for script

--- scripts/sparseimage/pp_sparse_json.sh
+++ scripts/sparseimage/pp_sparse_json.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Replace manifest part to the result of the sparse image building
+yum install -y epel-release
+yum install -y jq
 cat ${image_name}.json | jq --slurpfile newmanifest packer-manifest.json  '.manifest = $newmanifest' >> ${image_name}.json
