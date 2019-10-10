70ab02e05f2265ab1a8eb6dbc7b46b680c809d3a
Adam Horvath <adam.horvath@cloudera.com>
2019-10-10 15:03:33 +0200

    CB-3801 fix metadata generation again (#336)

--- scripts/pp_json.sh
+++ scripts/pp_json.sh
@@ -15,8 +15,8 @@ fi
 
 echo "pre_warm_parcels: ${pre_warm_parcels}"
 
-pre_warm_parcels=${pre_warm_parcels:-[[\"\"]]}
-pre_warm_csd=${pre_warm_csd:-\"\"}
+pre_warm_parcels=${pre_warm_parcels:-[ [ \"\" ] ]}
+pre_warm_csd=${pre_warm_csd:-[ \"\" ]}
 
 cat  > ${image_name}.json <<EOF
 {
