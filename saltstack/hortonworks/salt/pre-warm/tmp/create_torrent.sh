851718ae2c0c90524de69566e29bcb817af6dc17
Krisztian Horvath <keyki.kk@gmail.com>
2019-10-15 10:31:21 +0200

    CDPD-4775 include .skiphash file along with torrent creation

--- saltstack/hortonworks/salt/pre-warm/tmp/create_torrent.sh
+++ saltstack/hortonworks/salt/pre-warm/tmp/create_torrent.sh
@@ -4,10 +4,14 @@ set -x
 
 : ${PARCEL_REPO:=/opt/cloudera/parcel-repo}
 
-for parcel in $(ls -1 $PARCEL_REPO | grep .parcel | grep -v sha); do
-	mktorrent -l 19 -v -p -a "" -o "${PARCEL_REPO}/${parcel}.torrent" "${PARCEL_REPO}/${parcel}";
+parcels=("$PARCEL_REPO"/*.parcel)
+
+for parcel in "${parcels[@]}"; do
+	mktorrent -l 19 -v -p -a "" -o "${parcel}.torrent" "${parcel}";
+	touch "${parcel}".skiphash
 done
 
 chown cloudera-scm:cloudera-scm ${PARCEL_REPO}/*.torrent
+chown cloudera-scm:cloudera-scm ${PARCEL_REPO}/*.skiphash
 
 echo "Torrent creation successful" >> /tmp/create_torrent.status
