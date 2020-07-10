3126e3502b9bd4c5d92e3c4de28b2c3faeeb6e14
Zoltan Kornel Torok <ztorok@cloudera.com>
2020-07-10 17:01:03 +0200

    Revert "Update python 3.6 + Salt 3000.2"
    This reverts commit 61cd6a5600afcb60ac9cf7520ef1bf073de9a79f.

--- scripts/salt-install.sh
+++ scripts/salt-install.sh
@@ -6,11 +6,8 @@
 set -ex -o pipefail -o errexit
 
 function install_salt_with_pip() {
-  echo "Installing salt with version: $SALT_VERSION"
   pip install --upgrade pip
   pip install virtualenv
-  # fix pip3 not installing virtualenv for root
-  ln -s /usr/local/bin/virtualenv /usr/bin/virtualenv
   mkdir ${SALT_PATH}
   virtualenv ${SALT_PATH}
   source ${SALT_PATH}/bin/activate
@@ -106,26 +103,9 @@ function install_python_pip() {
   if [ "${OS_TYPE}" == "amazonlinux" ]; then
     yum install -y python27-devel python27-pip
   elif [ "${OS_TYPE}" == "redhat7" ] || [ "${OS_TYPE}" == "amazonlinux2" ] ; then
-    echo "Installing python36 with deps"
-    yum install -y python36 python36-pip python36-devel python36-setuptools
-    make_pip3_default_pip
-  elif [ "${OS_TYPE}" == "redhat8" ]; then
-    echo "Installing python36 with deps"
-    yum install -y python36 python36-devel
-    make_pip3_default_pip
+    yum install -y python2-pip python2-devel
   else
     yum install -y python-pip python-devel
-  fi
-}
-
-function make_pip3_default_pip() {
-  FILE=/bin/pip
-  if [ -f "$FILE" ] && ["${OS_TYPE}" != "redhat8" ]; then
-      mv /bin/pip /bin/pip2
-  fi
-  mv /bin/pip3 /bin/pip
-  if [ -f "$FILE" ]; then
-      mv /bin/pip3.6 /bin/pip
   fi
 }
 
