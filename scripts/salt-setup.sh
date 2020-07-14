99d0a2ede59857fa21988feb005e9691bc6e3084
Siddharth Seth <sseth@cloudera.com>
2020-07-13 23:53:22 -0700

    CB-7443. Changes to make some improvements to the time it takes to scale up nodes. - Introduces 2 pre-populates salt 'roles' in prewarmed images / images containing fluent. - Remove some unused salt modules which are slow to load.

--- scripts/salt-setup.sh
+++ scripts/salt-setup.sh
@@ -46,6 +46,37 @@ function apply_optional_states {
   fi
 }
 
+# Appends any prewarmed roles to a (non-salt) file, one role per line. salt-bootstrap takes care of setting up these roles during node setup.
+function add_single_role_for_cluster_salt {
+  local role=${1}
+  echo "${role}" >> /etc/salt/prewarmed_roles
+}
+
+function add_prewarmed_roles {
+  if [ "${INCLUDE_FLUENT}" == "Yes" ]; then
+    # Note: This will need to be changed if making changes to versions etc in the prewarmed image.
+    local fluent_prewarmed=${FLUENT_PREWARM_TAG}
+    echo "Adding ${fluent_prewarmed} to the list of roles for the final image"
+    add_single_role_for_cluster_salt ${fluent_prewarmed}
+  fi
+
+  if  [ "${STACK_TYPE}" == "CDH" -a ! -z "${CLUSTERMANAGER_VERSION}" -a ! -z "${CLUSTERMANAGER_BASEURL}" -a ! -z "${CLUSTERMANAGER_GPGKEY}" -a ! -z "${STACK_VERSION}" -a ! -z "${STACK_BASEURL}" -a ! -z "${STACK_REPOID}" ]; then
+    local prewarmed=${PREWARM_TAG}
+    echo "Adding ${prewarmed} to the list of roles for the final image"
+    add_single_role_for_cluster_salt ${prewarmed}
+  fi
+}
+
+function delete_unnecessary_files() {
+  # Salt (version as of this change: 3000.2) ends up taking a long time to load modules. vspere is an especially slow one taking 3 seconds.
+  # Salt does not seem to allow skipping module load ('disable_modules' only disables module usage, not module loading)
+  # So, deleting some modules which are not used, and tend to cause Exceptions / delays
+  find /opt/salt_3000.2/lib/python3.6/site-packages/salt/modules/ -name "*lxd*" -exec rm -f {} \;
+  find /opt/salt_3000.2/lib/python3.6/site-packages/salt/modules/ -name "*vsphere*" -exec rm -f {} \;
+  find /opt/salt_3000.2/lib/python3.6/site-packages/salt/modules/ -name "*boto3_elasticsearch*" -exec rm -f {} \;
+  find /opt/salt_3000.2/lib/python3.6/site-packages/salt/modules/ -name "*win_*" -exec rm -f {} \;
+}
+
 : ${CUSTOM_IMAGE_TYPE:=$1}
 
 case ${CUSTOM_IMAGE_TYPE} in
@@ -74,5 +105,11 @@ esac
 
 apply_optional_states
 
+echo "Adding prewarmed roles for salt used in final image"
+add_prewarmed_roles
+
 echo "Running validation and cleanup"
 highstate "final"
+
+echo "Deleting some  unnecessary files ..."
+delete_unnecessary_files
