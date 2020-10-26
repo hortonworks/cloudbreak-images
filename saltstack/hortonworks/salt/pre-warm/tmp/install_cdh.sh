1a266d2e4e6b5deed14f1f2e4bcf112ece637ae4
Peter Kedvessy <9048479+pkedvessy@users.noreply.github.com>
2020-10-26 16:14:20 +0100

    Revert "CB-9459 SHA checksum should not be copied as the target is the file itself."
    This reverts commit fc04cdc526832121c024f1bea807ccf7ad2ddf7c.

--- saltstack/hortonworks/salt/pre-warm/tmp/install_cdh.sh
+++ saltstack/hortonworks/salt/pre-warm/tmp/install_cdh.sh
@@ -20,9 +20,7 @@ verify_parcel_checksum() {
 
   echo "Downloading sha file from $STACK_BASEURL/${PARCELS_NAME}.$SHA_TYPE"
    curl -s -S "${STACK_BASEURL}/${PARCELS_NAME}.${SHA_TYPE}" -o "$PARCEL_REPO/$PARCELS_NAME.$SHA_TYPE"
-   if [ "$SHA_TYPE" != "sha" ]; then
-     cp "$PARCEL_REPO/$PARCELS_NAME.$SHA_TYPE" "$PARCEL_REPO/$PARCELS_NAME.sha"
-   fi
+   cp "$PARCEL_REPO/$PARCELS_NAME.$SHA_TYPE" "$PARCEL_REPO/$PARCELS_NAME.sha"
    sed "s/$/  ${PARCELS_NAME}/" "$PARCEL_REPO/$PARCELS_NAME.$SHA_TYPE" |
      tee "$PARCEL_REPO/$PARCELS_NAME.shacheck" > /dev/null
 
