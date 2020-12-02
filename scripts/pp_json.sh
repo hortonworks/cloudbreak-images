c81149e282e830d94efa7fd6df8c4ffd2767626b
Jozsef Szeles <jozsef.szeles@cloudera.com>
2020-12-02 15:21:24 +0100

    CB-9872: Remove the ambari, hdp, hdf relevant parts from the salt stack of images repository

--- scripts/pp_json.sh
+++ scripts/pp_json.sh
@@ -12,7 +12,7 @@ if [ -f package-versions.json -a "$stack_version" != "" -a "$clustermanager_vers
             cat package-versions.json | jq --arg stack_version $stack_version --arg clustermanager_version $clustermanager_version --arg cfm_build_number $cfm_build_number --arg csp_build_number $csp_build_number --arg profiler_build_number $profiler_build_number --arg spark3_build_number $spark3_build_number --arg csa_build_number $csa_build_number --arg cm_build_number $cm_build_number --arg stack_build_number $stack_build_number '. += {"stack" : $stack_version,  "cm" : $clustermanager_version,  "cm-build-number" : $cm_build_number,  "cdh-build-number" : $stack_build_number, "cfm" : $cfm_build_number, "csp" : $csp_build_number, "profiler" : $profiler_build_number, "spark3": $spark3_build_number, "csa": $csa_build_number}' > package-versions-tmp.json && mv package-versions-tmp.json package-versions.json
         fi
 	else
-		cat package-versions.json | jq --arg stack_version $stack_version --arg clustermanager_version $clustermanager_version '. += {"stack" : $stack_version,  "ambari" : $clustermanager_version}' > package-versions-tmp.json && mv package-versions-tmp.json package-versions.json
+		cat package-versions.json | jq --arg stack_version $stack_version --arg clustermanager_version $clustermanager_version '. += {"stack" : $stack_version }' > package-versions-tmp.json && mv package-versions-tmp.json package-versions.json
 	fi
 	cat package-versions.json
 fi
@@ -39,31 +39,19 @@ cat  > ${image_name}_$metadata_filename_postfix.json <<EOF
 "image_name": "${image_name}",
 "description": "${description}",
 "stack_type": "${stack_type}",
-"ambari_version": "${clustermanager_version}",
-"ambari_os_type": "${clustermanager_os_type}",
-"ambari_baseurl": "$([ "$repository_type" == "local" ] && echo "${local_url_ambari}" || echo "${clustermanager_baseurl}")",
 "cm_gpgkey": "${clustermanager_gpgkey}",
 "cm_version": "${clustermanager_version}",
 "cm_os_type": "${clustermanager_os_type}",
 "cm_baseurl": "${clustermanager_baseurl}",
-"hdp_version": "${stack_version}",
-"hdp_os_type": "${stack_os_type}",
-"hdp_baseurl": "$([ "$repository_type" == "local" ] && echo "${local_url_hdp}" || echo "${stack_baseurl}")",
-"hdp_repoid": "${stack_repoid}",
 "cdh_version": "${stack_version}",
 "cdh_os_type": "${stack_os_type}",
 "cdh_baseurl": "${stack_baseurl}",
 "cdh_repoid": "${stack_repoid}",
-"hdputil_version": "${hdputil_version}",
-"hdputil_os_type": "${hdputil_os_type}",
-"hdputil_baseurl": "$([ "$repository_type" == "local" ] && echo "${local_url_hdp_utils}" || echo "${hdputil_baseurl}")",
-"hdputil_repoid": "${hdputil_repoid}",
 "mpack_urls": $(if [[ -z "$mpack_urls" ]]; then echo []; else mpackjson=$(IFS=, read -ra mpacks <<< "$mpack_urls"; for mpack in "${mpacks[@]}"; do echo "\"${mpack}\","; done); echo "[${mpackjson:0:${#mpackjson}-1}]"; fi),
 "aws_ami_regions": "${aws_ami_regions}",
 "azure_storage_accounts": "${azure_storage_accounts}",
 "gcp_storage_bundle": "${gcp_storage_bundle}",
 "gcp_ami_regions": "${gcp_ami_regions}",
-"hdp_repository_version": "${stack_repository_version}",
 "cdh_repository_version": "${stack_repository_version#*-}",
 "cm_build_number": "${cm_build_number}",
 "stack_build_number": "${stack_build_number}",
