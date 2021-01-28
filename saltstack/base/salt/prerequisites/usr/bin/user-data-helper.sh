ff3b07f7b31f9fc90999f9954b9c2a1c41e4bc53
TheTinkerDad <laszlo.merczel@cloudera.com>
2021-01-28 02:08:22 +0100

    CB-10784: Replaced localhost with 127.0.0.1 as it caused problems with nginx

--- saltstack/base/salt/prerequisites/usr/bin/user-data-helper.sh
+++ saltstack/base/salt/prerequisites/usr/bin/user-data-helper.sh
@@ -118,9 +118,9 @@ setup_tls() {
   fi
 
   if [[ "$IS_CCM_V2_ENABLED" == "true" ]]; then
-    echo "CCMv2 is enabled while creating an environment so ssl client verification is turned off and localhost:9443 used"
+    echo "CCMv2 is enabled while creating an environment so ssl client verification is turned off and 127.0.0.1:9443 used"
     sed -i -E "s/ssl_verify_client(\s)+on;/ssl_verify_client off;/" /etc/nginx/sites-enabled/ssl-template
-    sed -i -E "s/listen(\s)+9443;/listen       localhost:9443;/" /etc/nginx/sites-enabled/ssl-template
+    sed -i -E "s/listen(\s)+9443;/listen       127.0.0.1:9443;/" /etc/nginx/sites-enabled/ssl-template
   fi
 }
 
