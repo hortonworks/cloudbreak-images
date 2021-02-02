877ee9b23e1967a11a2f1ccb321c4dfd02f11861
SachinHg <sachin0892@gmail.com>
2021-02-02 20:22:19 +0530

    CB-10969 | Update the CB user data script and corresponding files to include an argument to support http proxy while running inverting proxy agent. (#511)

--- saltstack/base/salt/ccmv2-inverting-proxy-agent/cdp/bin/ccmv2/update-inverting-proxy-agent-values.sh
+++ saltstack/base/salt/ccmv2-inverting-proxy-agent/cdp/bin/ccmv2/update-inverting-proxy-agent-values.sh
@@ -5,10 +5,11 @@
 
 ARG_ERR=0
 
-if [[ "$#" -lt 7 || "$#" -gt 9 ]]; then
-  echo "Wrong number of arguments passed. The script requires 8 arguments to be passed for 'BACKEND_ID', 'BACKEND_HOST', 'BACKEND_PORT', 'AGENT_KEY_PATH', 'AGENT_CERT_PATH', 'TRUSTED_BACKEND_CERT_PATH' AND 'TRUSTED_PROXY_CERT_PATH'."
+if [[ "$#" -lt 7 || "$#" -gt 10 ]]; then
+  echo "Wrong number of arguments passed. The script requires 7 arguments to be passed for 'BACKEND_ID', 'BACKEND_HOST', 'BACKEND_PORT', 'AGENT_KEY_PATH', 'AGENT_CERT_PATH', 'TRUSTED_BACKEND_CERT_PATH' AND 'TRUSTED_PROXY_CERT_PATH'."
   echo "An optional 8th argument for 'INVERTING_PROXY_URL' can be passed. If not passed, the environment variable INVERTING_PROXY_URL will be used."
-  echo "An optional 9th argument for 'SCHEME' can be passed. If not passed, the value 'https' will be used."
+  echo "An optional 9th argument for 'HTTP_PROXY_URL' can be passed. If not passed, no proxy will be used."
+  echo "An optional 10th argument for 'SCHEME' can be passed. If not passed, the value 'https' will be used."
   ARG_ERR=1
 fi
 
@@ -19,7 +20,9 @@ if [ -z "$INVERTING_PROXY_URL" ]; then
   ARG_ERR=1
 fi
 
-SCHEME=${9:-https}
+HTTP_PROXY_URL=${9:-""}
+
+SCHEME=${10:-https}
 
 if [ $ARG_ERR -eq 1 ] ; then
   exit 1
@@ -44,6 +47,7 @@ AGENT_CERT_PATH=${AGENT_CERT_PATH}
 TRUSTED_BACKEND_CERT_PATH=${TRUSTED_BACKEND_CERT_PATH}
 TRUSTED_PROXY_CERT_PATH=${TRUSTED_PROXY_CERT_PATH}
 SCHEME=${SCHEME}
+HTTP_PROXY_URL=${HTTP_PROXY_URL}
 EOF
 
 if [ -f "$VALUES_FILE" ]; then
