b2aaf9d3eea0a777da5764c697d5ebcfb3961458
Jozsef Szeles <jozsef.szeles@cloudera.com>
2021-08-17 12:36:16 +0200

    Add inverting-proxy-agent to package versions in catalog

--- saltstack/final/salt/cleanup/tmp/generate-package-versions.sh
+++ saltstack/final/salt/cleanup/tmp/generate-package-versions.sh
@@ -5,6 +5,12 @@ set -x
 echo '{}' | jq --arg sb "$(salt-bootstrap --version | awk '{print $2}')" '. + {"salt-bootstrap": $sb}' > /tmp/package-versions.json
 cat /tmp/package-versions.json | jq --arg sv "$($SALT_PATH/bin/salt-call --local grains.get saltversion --out json | jq -r .local)" '. + {"salt": $sv}' > /tmp/package-versions.json
 
+cat /tmp/package-versions.json | jq --arg inverting_proxy_agent_version "$(jumpgate-agent --version | awk 'NR==1{print $3}')" '. + {"inverting-proxy-agent": $inverting_proxy_agent_version}' > /tmp/package-versions.json
+JUMPGATE_AGENT_GBN_REGEX=".*\/([0-9]+)\/.*"
+if [[ $JUMPGATE_AGENT_RPM_URL =~ $JUMPGATE_AGENT_GBN_REGEX ]]; then
+    cat /tmp/package-versions.json | jq --arg inverting_proxy_agent_gbn ${BASH_REMATCH[1]} '. + {"inverting-proxy-agent_gbn": $inverting_proxy_agent_gbn}' > /tmp/package-versions.json.tmp && mv /tmp/package-versions.json.tmp /tmp/package-versions.json
+fi
+
 if [[ "$CDP_TELEMETRY_VERSION" != "" ]]; then
 	cat /tmp/package-versions.json | jq --arg cdp_telemetry_version $CDP_TELEMETRY_VERSION -r '. + {"cdp-telemetry": $cdp_telemetry_version}' > /tmp/package-versions.json.tmp && mv /tmp/package-versions.json.tmp /tmp/package-versions.json
 fi
