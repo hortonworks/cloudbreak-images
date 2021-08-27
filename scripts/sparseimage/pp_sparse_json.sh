fb1ae655e504e5cb8cdb2fd9d71b2a1bbf8ecddc
David Bajzath <bajzathd@gmail.com>
2021-08-27 18:04:42 +0200

    Download jq from a https source

--- scripts/sparseimage/pp_sparse_json.sh
+++ scripts/sparseimage/pp_sparse_json.sh
@@ -4,7 +4,9 @@ set -xe
 # Replace manifest part to the result of the sparse image building
 ls -al *.json
 echo Image name is: $image_name 
-apk update && apk add jq
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x jq-linux64
+mv jq-linux64 /bin/jq
 cat packer-manifest.json
 newartifacts=$(cat packer-manifest.json | jq -r '.builds[0].artifact_id')
 cat ${image_name}_$metadata_filename_postfix.json | jq --arg artifacts "$newartifacts"  '.manifest.builds[0].artifact_id  = $artifacts' >> temp.json
