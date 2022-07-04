5089cbda1f0a1705a96492f01f4d9d0dfe8a095c
pkedvessy <9048479+pkedvessy@users.noreply.github.com>
2022-07-04 10:56:07 +0200

    CB-17496 Fix RHEL8 image burning
    * Use us-west1 repository as ddpt install fails with the main one
    * Update repositories before installing ddpt
    * Update mount offset as the new Ubuntu has a boot partition

--- scripts/sparseimage/sparse_image.sh
+++ scripts/sparseimage/sparse_image.sh
@@ -5,14 +5,19 @@ if [ -f /etc/redhat-release ]; then
     sudo yum install -y epel-release 
     sudo yum install -y -q ddpt 
 else
+    sudo sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/us-west-1.ec2.archive.ubuntu.com/' /etc/apt/sources.list
+    sudo apt-get clean
+    sudo apt-get update
     sudo apt-get install -y ddpt
 fi
 
+sudo sysctl kernel.dmesg_restrict=0
 sudo mkdir /image
 sudo dd if=/dev/xvdb of=/image/sparse.img bs=1M status=progress
 blkid /image/sparse.img
+sudo fdisk -l /image/sparse.img
 sudo mkdir /loop
-sudo mount -t xfs -o loop,discard,offset=1048576 -o nouuid /image/sparse.img /loop || dmesg | tail -n 50
+sudo mount -t xfs -o loop,discard,offset=2097152 -o nouuid /image/sparse.img /loop || dmesg | tail -n 50
 sudo fstrim /loop
 sudo umount /loop
 sudo ddpt of=/dev/xvdc if=/image/sparse.img bs=1M oflag=sparse
