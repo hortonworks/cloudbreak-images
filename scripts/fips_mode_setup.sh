90412ebd3e87f3d855fe74365b8c0b20b83f1f30
pkedvessy <9048479+pkedvessy@users.noreply.github.com>
2023-03-29 17:13:35 +0200

    CB-16336: Enable fips mode regardless the os and cloud provider

--- scripts/fips_mode_setup.sh
+++ scripts/fips_mode_setup.sh
@@ -2,8 +2,7 @@
 
 echo "FIPS mode setup"
 
-if [ "${OS}" == "redhat8" ] && [ "${CLOUD_PROVIDER}" == "AWS_GOV" ] ; then
+
     echo "Set FIPS enabled and then reboot..."
     fips-mode-setup --enable
     reboot
-fi
