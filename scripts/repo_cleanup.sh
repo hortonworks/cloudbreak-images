8a70dec7a54aa0abad73215edbc18b6dfa456260
David Bajzath <bajzathd@gmail.com>
2023-07-11 12:00:51 +0200

    CB-22420 Delete unused repos from RHEL8 on all public cloud providers

--- scripts/repo_cleanup.sh
+++ scripts/repo_cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
 
-if [[ "${OS}" == "redhat8" && "$CLOUD_PROVIDER" != "Azure"  && "$CLOUD_PROVIDER" != "AWS_GOV" ]] ; then
+if [[ "${OS}" == "redhat8" && "$CLOUD_PROVIDER" != "AWS_GOV" ]] ; then
     find /etc/yum.repos.d -type f ! -name 'pgdg-redhat-all.repo' ! -name 'rhel8.8_cldr_mirrors.repo' -delete
 fi
