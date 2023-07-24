5d80a6bda78926afcdc2857e0ab1b1b2fdaba32f
David Bajzath <dbajzath@cloudera.com>
2023-07-24 16:27:52 +0200

    CB-22248 Enable CIS control on Azure

--- scripts/cis_control.sh
+++ scripts/cis_control.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -xe
 
-if [[ "${OS}" == "redhat8" && "$CLOUD_PROVIDER" != "Azure"  && "$CLOUD_PROVIDER" != "AWS_GOV" ]] ; then
+if [[ "${OS}" == "redhat8" && "$CLOUD_PROVIDER" != "AWS_GOV" ]] ; then
     # Remediating the system to align with the CIS L1 baseline using an SSG Ansible playbook
     # The ansible playbook is available at https://github.com/AutomateCompliance/AnsibleCompliancePlaybooks/blob/main/rhel8-playbook-cis_server_l1.yml
 
     # The list of tags of ansible tasks from the above-mentioned playbook that would break functionality, so we are skipping temporarily
+    SKIP_TAGS="package_firewalld_installed,service_firewalld_enabled"
     if [ "${IMAGE_BASE_NAME}" == "freeipa" ] ; then
-        SKIP_TAGS=package_firewalld_installed,service_firewalld_enabled,service_httpd_disabled
-    else
-        SKIP_TAGS=package_firewalld_installed,service_firewalld_enabled
+        SKIP_TAGS+=",service_httpd_disabled"
+    fi
+    if [ "${CLOUD_PROVIDER}" == "Azure" ]; then
+        # Azure needs UDF to execute custom data: https://learn.microsoft.com/en-us/azure/virtual-machines/linux/using-cloud-init#cloud-init-vm-provisioning-without-a-udf-driver
+        SKIP_TAGS+=",kernel_module_udf_disabled"
     fi
 
     #Install and download what we need for the hardening
