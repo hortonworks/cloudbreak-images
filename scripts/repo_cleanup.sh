c153878a62456a6761887a98a0d6fd29c399b3b2
pkedvessy <9048479+pkedvessy@users.noreply.github.com>
2023-08-04 09:08:12 +0200

    ENGESC-21507 Fix RHEL8 related issues (#868)
    * ENGESC-21507 Fine tune CIS hardening: openldap-clinet should not be removed as it removes ipa-server as well
    
    * ENGESC-21507 Remove internal rhel8 repo mirror at the end of image burning

--- scripts/repo_cleanup.sh
+++ scripts/repo_cleanup.sh
@@ -2,5 +2,5 @@
 set -xe
 
 if [[ "${OS}" == "redhat8" && "$CLOUD_PROVIDER" != "AWS_GOV" ]] ; then
-    find /etc/yum.repos.d -type f ! -name 'pgdg-redhat-all.repo' ! -name 'rhel8.8_cldr_mirrors.repo' -delete
+    find /etc/yum.repos.d -type f ! -name 'pgdg-redhat-all.repo' -delete
 fi
