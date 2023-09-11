2cbd4c6f5d1dd84f463b075afa4687a6be94f1de
David Bajzath <dbajzath@cloudera.com>
2023-09-11 16:16:13 +0200

    CB-17933 Generate manifest for YARN images

---
+++ scripts/push-to-metadata-repo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+git clone git@github.com:$GITHUB_ORG/$GITHUB_REPO.git
+if [ "$CLOUD_PROVIDER" != "YARN" ]; then
+  FILE=$(ls -1tr *_manifest.json | tail -1 | sed "s/_manifest//")
+else
+  FILE=$(ls -1tr ${IMAGE_NAME}*.json)
+fi
+cp $FILE $GITHUB_REPO
+mkdir -p "${GITHUB_REPO}/manifest"
+UUID=$(cat $FILE | jq -r .uuid)
+if [ "$CLOUD_PROVIDER" != "YARN" ]; then
+  cp installed-delta-packages.csv "${GITHUB_REPO}/manifest/${UUID}-manifest.csv"
+  cp installed-full-packages.csv "${GITHUB_REPO}/manifest/${UUID}-full-manifest.csv"
+fi
+cd $GITHUB_REPO && git add -A && git commit -am "Upload new metadata files for ${UUID}" && git push
