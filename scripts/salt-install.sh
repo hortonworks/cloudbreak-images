0fcc38c7581d5fd0e6c091517b247afbc4b73a82
pkedvessy <9048479+pkedvessy@users.noreply.github.com>
2023-12-01 14:25:35 +0100

    CB-23970 Fail image burning in case of internal rhel repo is not available (#933)

--- scripts/salt-install.sh
+++ scripts/salt-install.sh
@@ -70,7 +70,7 @@ function update_yum_repos() {
     RHEL_VERSION=${RHEL_VERSION/.0/}
     REPO_FILE=rhel${RHEL_VERSION}_cldr_mirrors.repo
     rm /etc/yum.repos.d/*.repo -f
-    curl https://mirror.infra.cloudera.com/repos/rhel/server/8/${RHEL_VERSION}/${REPO_FILE} > /etc/yum.repos.d/${REPO_FILE}
+    curl https://mirror.infra.cloudera.com/repos/rhel/server/8/${RHEL_VERSION}/${REPO_FILE} --fail > /etc/yum.repos.d/${REPO_FILE}
 
     # Workaround on resolving the hostname as for some reason the DNS can't resolve it at provision time
     if [ "${IMAGE_BURNING_TYPE}" == "base" ] ; then
