d94cded90cd6c2ee412782b18a6ad903c2409e2d
David Bajzath <dbajzath@cloudera.com>
2023-12-15 14:32:58 +0100

    CB-24059 Update default Postgres versions
    Centos7
    	install 10,11,14
    	default 10
    RHEL8 AWS/GCP/Azure
    	install 11,14
    	default 14
    RHEL8 YARN
    	install 14
    	default 14

--- saltstack/base/salt/postgresql/scripts/conf_pgconfig_path.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Updating /etc/environment to include PostgreSQL binaries on the path..."    
-cat /etc/environment
-sed -e '/^PATH/s/"$/:\/usr\/pgsql-11\/bin"/g' -i /etc/environment
-cat /etc/environment
