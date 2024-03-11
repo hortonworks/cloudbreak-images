ebabbd619814e419aafea43f192df25bc7419445
Laszlo Merczel <laszlo.merczel@cloudera.com>
2024-03-11 19:26:35 +0100

    CB-24945: Added temporary RHEL 8.9 repo file

--- scripts/salt-install.sh
+++ scripts/salt-install.sh
@@ -70,7 +70,40 @@ function update_yum_repos() {
     RHEL_VERSION=${RHEL_VERSION/.0/}
     REPO_FILE=rhel${RHEL_VERSION}_cldr_mirrors.repo
     rm /etc/yum.repos.d/*.repo -f
-    curl https://mirror.infra.cloudera.com/repos/rhel/server/8/${RHEL_VERSION}/${REPO_FILE} --fail > /etc/yum.repos.d/${REPO_FILE}
+    
+    # Temporary workaround for missing RHEL 8.9 repo file
+    # curl https://mirror.infra.cloudera.com/repos/rhel/server/8/${RHEL_VERSION}/${REPO_FILE} --fail > /etc/yum.repos.d/${REPO_FILE}
+    cat <<EOF >/etc/yum.repos.d/${REPO_FILE}
+# publish this file under /srv/mirrors/repos/rhel/server/8/8.9/
+
+[ubi-8.9-baseos-cldr]
+name = Red Hat Universal Base Image 8.9 (RPMs) - BaseOS CLDR
+baseurl = http://mirror.infra.cloudera.com/repos/rhel/server/8/8.9/x86_64/baseos/os
+enabled = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+gpgcheck = 1
+
+[ubi-8.9-appstream-cldr]
+name=Cloudera Rhel 8.9 appsteam mirrors
+baseurl=http://mirror.infra.cloudera.com/repos/rhel/server/8/8.9/x86_64/appstream/os
+enabled=1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+gpgcheck = 1
+
+[ubi-8.9-supplementary-cldr]
+name=Cloudera Rhel 8.9 appsteam mirrors
+baseurl=http://mirror.infra.cloudera.com/repos/rhel/server/8/8.9/x86_64/supplementary/os
+enabled=1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+gpgcheck = 1
+
+[ubi-8.9-codeready-builder-cldr]
+name=Cloudera Rhel 8.9 codeready-builder mirror
+baseurl=http://mirror.infra.cloudera.com/repos/rhel/server/8/8.9/x86_64/codeready-builder/os
+enabled=1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+gpgcheck = 1
+EOF
 
     # Workaround on resolving the hostname as for some reason the DNS can't resolve it at provision time
     if [ "${IMAGE_BURNING_TYPE}" == "base" ] ; then
