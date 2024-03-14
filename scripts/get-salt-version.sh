29d9a414224e4668ebdb971a602c4504dfd9091c
David Bajzath <dbajzath@cloudera.com>
2024-03-14 12:06:07 +0100

    CB-23061 Move temp files to attached disk to save space
    Temp files moved:
     - Parcels (empty files were left in place to make CM happy)
     - Ansible stuff needed for cis hardening
    
    Storage requirements changed for RHEL8:
     - Azure needs to 64GB, used to require 90GB for 7.2.18 (LVMs of the source image can not be shrinked so this is the minimum)
     - CDH version 7.2.18+ needs 52GB, used to require 72GB
     - Older runtimes need 48GB, used to require 64GB
    
    Other improvements:
     - Upgrade to latest Packer
     - Move image size calculation to separate script
     - Show storage usage at the end of image burning
     - Remove --retcode-passthrough from salt highstates to fail on error

--- scripts/get-salt-version.sh
+++ scripts/get-salt-version.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-compare_version () {
-  if [[ $1 == $2 ]]; then
-    return 0
-  fi
-  local IFS=.
-  local i version1=($1) version2=($2)
-  # Fill empty fields in version1 with zeros
-  for ((i=${#version1[@]}; i<${#version2[@]}; i++))
-  do
-    version1[i]=0
-  done
-  for ((i=0; i<${#version1[@]}; i++))
-  do
-    # Fill empty fields in version2 with zeros
-    if [[ -z ${version2[i]} ]]; then
-      version2[i]=0
-    fi
-    if ((10#${version1[i]} > 10#${version2[i]})); then
-      return 1
-    fi
-    if ((10#${version1[i]} < 10#${version2[i]})); then
-      return 2
-    fi
-  done
-  return 0
-}
+source scripts/utils.sh
 
 BASE_NAME=$1
 STACK_VERSION=$2
--- scripts/salt-setup.sh
+++ scripts/salt-setup.sh
@@ -45,7 +45,7 @@ function apply_rhel8_salt_patch {
 function highstate {
   local saltenv=${1}
   copy_resources ${saltenv}
-  ${SALT_PATH}/bin/salt-call --no-color --local state.highstate saltenv=${saltenv} --retcode-passthrough -l info --log-file=/tmp/salt-build-${saltenv}.log --log-file-level=info --config-dir=/tmp/saltstack/config
+  ${SALT_PATH}/bin/salt-call --no-color --local state.highstate saltenv=${saltenv} -l info --log-file=/tmp/salt-build-${saltenv}.log --log-file-level=info --config-dir=/tmp/saltstack/config
 }
 
 function apply_optional_states {
@@ -55,7 +55,7 @@ function apply_optional_states {
   then
     local saltenv="optional"
     copy_resources ${saltenv}
-    ${SALT_PATH}/bin/salt-call --no-color --local state.sls ${OPTIONAL_STATES} saltenv=${saltenv} pillarenv=${saltenv} --retcode-passthrough -l info --log-file=/tmp/salt-build-${saltenv}.log --config-dir=/tmp/saltstack/config
+    ${SALT_PATH}/bin/salt-call --no-color --local state.sls ${OPTIONAL_STATES} saltenv=${saltenv} pillarenv=${saltenv} -l info --log-file=/tmp/salt-build-${saltenv}.log --config-dir=/tmp/saltstack/config
   fi
 }
 
