96e5cf630b35739b33a63fa221821247fcc3409f
David Bajzath <dbajzath@cloudera.com>
2024-07-08 12:39:36 +0200

    COMOPS-3628 Arm64 rhel8 repo

--- scripts/salt-install.sh
+++ scripts/salt-install.sh
@@ -67,13 +67,15 @@ function update_yum_repos() {
   if [[ "${OS}" == "redhat8" ]]; then
     # Remove RHEL official repos and use the internal mirror in case of RHEL8
     if [[ "${CLOUD_PROVIDER}" != "AWS_GOV" ]]; then
-      if [[ "${ARCHITECTURE}" != "arm64" ]]; then
-        # Internal repo is not yet available for AWS_GOV and arm64 images
-        RHEL_VERSION=$(cat /etc/redhat-release | grep -oP "[0-9\.]*")
-        RHEL_VERSION=${RHEL_VERSION/.0/}
-        REPO_FILE=rhel${RHEL_VERSION}_cldr_mirrors.repo
-        rm /etc/yum.repos.d/*.repo -f
-        curl https://mirror.infra.cloudera.com/repos/rhel/server/8/${RHEL_VERSION}/${REPO_FILE} --fail > /etc/yum.repos.d/${REPO_FILE}
+      # Internal repo is not yet available for AWS_GOV images
+      RHEL_VERSION=$(cat /etc/redhat-release | grep -oP "[0-9\.]*")
+      RHEL_VERSION=${RHEL_VERSION/.0/}
+      REPO_FILE=rhel${RHEL_VERSION}_cldr_mirrors.repo
+      rm /etc/yum.repos.d/*.repo -f
+      curl https://mirror.infra.cloudera.com/repos/rhel/server/8/${RHEL_VERSION}/${REPO_FILE} --fail > /etc/yum.repos.d/${REPO_FILE}
+      if [[ "${ARCHITECTURE}" == "arm64" ]]; then
+        # ubi-8.8-supplementary-cldr and ubi-8.8-codeready-builder-cldr are not yet available for arm64
+        sed -i '16,$ d' /etc/yum.repos.d/${REPO_FILE}
       fi
 
       # Workaround on resolving the hostname as for some reason the DNS can't resolve it at provision time
