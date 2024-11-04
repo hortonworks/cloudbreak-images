9f99714a741a5a77a767c8885efefa29bd197547
Laszlo Merczel <laszlo.merczel@cloudera.com>
2024-11-05 00:15:38 +0100

    CB-27371: Remove unnecessary PG repo file from images

--- scripts/repo_cleanup.sh
+++ scripts/repo_cleanup.sh
@@ -6,8 +6,8 @@ if [[ "${OS}" == "redhat8" && "$CLOUD_PROVIDER" != "AWS_GOV" ]] ; then
         RHEL_VERSION=$(cat /etc/redhat-release | grep -oP "[0-9\.]*")
         RHEL_VERSION=${RHEL_VERSION/.0/}
         REPO_FILE=rhel${RHEL_VERSION}_cldr_mirrors.repo
-        find /etc/yum.repos.d -type f ! -name 'pgdg-redhat-all.repo' ! -name $REPO_FILE -delete
+        find /etc/yum.repos.d -type f ! -name $REPO_FILE -delete
     else
-        find /etc/yum.repos.d -type f ! -name 'pgdg-redhat-all.repo' -delete
+        find /etc/yum.repos.d -type f -delete
     fi
 fi
