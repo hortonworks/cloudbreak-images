539652a74c8857c225a1eda45d65a37c83088165
Mészáros Gergely <gmeszaros@cloudera.com>
2024-11-12 15:45:08 +0100

    CB-26272 - Cluster side: Ensure that /var/log/user-data.log does not reveal any secrets

--- saltstack/base/salt/prerequisites/usr/bin/ccmv2-helper.sh
+++ saltstack/base/salt/prerequisites/usr/bin/ccmv2-helper.sh
@@ -4,6 +4,14 @@ setup_ccmv2() {
   : ${CCM_V2_AGENT_KEY_ID:? required}
   : ${CCM_V2_AGENT_BACKEND_ID_PREFIX:? required}
 
+ACCESS_KEY_PATH="/etc/ccmv2/access_key"
+touch $ACCESS_KEY_PATH
+chmod 600 "$ACCESS_KEY_PATH"
+
+cat > ${ACCESS_KEY_PATH} <<EOF
+CCM_V2_AGENT_ACCESS_KEY_ID=$CCM_V2_AGENT_ACCESS_KEY_ID
+EOF
+
   BACKEND_ID="${CCM_V2_AGENT_BACKEND_ID_PREFIX}${INSTANCE_ID}"
   BACKEND_HOST="localhost"
   BACKEND_PORT="9443"
@@ -34,11 +42,15 @@ setup_ccmv2() {
   INVERTING_PROXY_URL="$CCM_V2_INVERTING_PROXY_HOST"
 
   if [[ "$IS_CCM_V2_JUMPGATE_ENABLED" == "true" && "$IS_FREEIPA" == "true" && ! -z $CCM_V2_AGENT_ACCESS_KEY_ID ]]; then
-    ACCESS_KEY_ID="$CCM_V2_AGENT_ACCESS_KEY_ID"
 
     if [[ -z $CCM_V2_AGENT_HMAC_KEY ]]; then
       # legacy without HMAC check, AES-128
+      set +x
       ACCESS_KEY="$(echo ${CCM_V2_AGENT_ENCIPHERED_ACCESS_KEY} | openssl enc -aes-128-cbc -d -A -a -K ${CCM_V2_AGENT_KEY_HEX} -iv ${LEGACY_IV})"
+      cat >>${ACCESS_KEY_PATH} <<EOF
+ACCESS_KEY=$ACCESS_KEY
+EOF
+      set -x
     else
       # HMAC check, AES-256
       : ${CCM_V2_IV:? required}
@@ -49,9 +61,14 @@ setup_ccmv2() {
         echo "HMAC for Machine User Private Key does not match the calculated value. Exiting."
         exit 1
       fi
+      set +x
       CCM_V2_AGENT_KEY_HEX32=$(echo -n $CCM_V2_AGENT_KEY_ID | od -An -tx1 -N32 | tr -d ' \n')
       CCM_V2_IV_HEX=$(echo -n $CCM_V2_IV | od -An -tx1 -N16 | tr -d ' \n')
       ACCESS_KEY="$(echo ${CCM_V2_AGENT_ENCIPHERED_ACCESS_KEY} | openssl enc -aes-256-cbc -d -A -a -K ${CCM_V2_AGENT_KEY_HEX32} -iv ${CCM_V2_IV_HEX})"
+      cat >>${ACCESS_KEY_PATH} <<EOF
+ACCESS_KEY=$ACCESS_KEY
+EOF
+      set -x
     fi
 
   fi
@@ -59,5 +76,5 @@ setup_ccmv2() {
   # A more sophisticated solution might need to be patched in later - tbh the script originally expected a full url with protocol scheme and closing slash
   INVERTING_PROXY_FULL_URL="https://$INVERTING_PROXY_URL/"
 
-  /cdp/bin/ccmv2/generate-config.sh "$BACKEND_ID" "$BACKEND_HOST" "$BACKEND_PORT" "$AGENT_KEY_PATH" "$AGENT_CERT_PATH" "$ENVIRONMENT_CRN" "$ACCESS_KEY_ID" "$ACCESS_KEY" "$TRUSTED_BACKEND_CERT_PATH" "$TRUSTED_PROXY_CERT_PATH" "$INVERTING_PROXY_FULL_URL"
+  /cdp/bin/ccmv2/generate-config.sh "$BACKEND_ID" "$BACKEND_HOST" "$BACKEND_PORT" "$AGENT_KEY_PATH" "$AGENT_CERT_PATH" "$ENVIRONMENT_CRN" "$ACCESS_KEY_PATH" "$TRUSTED_BACKEND_CERT_PATH" "$TRUSTED_PROXY_CERT_PATH" "$INVERTING_PROXY_FULL_URL"
 }
