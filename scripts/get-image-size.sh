0763683489bcda8470052c1beeb5263c21b33ffe
David Bajzath <dbajzath@cloudera.com>
2024-11-21 14:52:27 +0100

    CB-27926 Optimize image sizes

--- scripts/get-image-size.sh
+++ scripts/get-image-size.sh
@@ -14,19 +14,25 @@ if [ "$OS" == "centos7" ]; then
     IMAGE_SIZE=36
   fi
 else
-  compare_version $STACK_VERSION 7.2.18
-  COMP_RESULT=$?
-  if [[ "$CLOUD_PROVIDER" == "Azure" ]]; then
-    if [[ "$ARCHITECTURE" == "arm64" ]]; then
+  if [[ -n "$STACK_VERSION" ]]; then
+    # runtime image
+    compare_version $STACK_VERSION 7.2.18
+    COMP_RESULT=$?
+    if [[ "$CLOUD_PROVIDER" == "Azure" ]]; then
+      IMAGE_SIZE=70
+    elif [[ $COMP_RESULT -lt 2 ]]; then
+      # stack version >= 7.2.18
+      IMAGE_SIZE=56
+    else
+      IMAGE_SIZE=48
+    fi
+  else
+    # freeipa and base image
+    if [[ "$CLOUD_PROVIDER" == "Azure" ]]; then
       IMAGE_SIZE=64
     else
-      IMAGE_SIZE=70
+      IMAGE_SIZE=48
     fi
-  elif [[ $COMP_RESULT -lt 2 ]]; then
-    # stack version >= 7.2.18
-    IMAGE_SIZE=56
-  else
-    IMAGE_SIZE=48
   fi
 fi
 
