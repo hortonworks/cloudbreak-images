0fe5d99942b4eb4c1931427ac9223882f2d4ea81
Lajos Rodek <15786876+lajosrodek@users.noreply.github.com>
2025-01-27 14:31:06 +0100

    CB-28300 Generate LUKS volume key using `kms:GenerateRandom`
    * `create-luks-volume.sh`:
      * Pass a manually obtained LUKS volume key to `cryptsetup luksFormat`.
      * The volume key is generated using `kms:GenerateRandom` with size 64
        bytes / 512 bits.
      * The plaintext volume key file is kept on the same `tmpfs` volume
        already used for the plaintext passphrase.
      * Since the volume key in plaintext form is only needed at the time of
        `cryptsetup luksFormat` invocation, the volume key file gets
        immediately removed after the successful completion of this command.
    * `cdp-retrieve-userdata-secrets.sh`, `create-luks-volume.sh`,
      `populate-luks-volume.sh`, `reopen-luks-volume.sh`:
      * Prefix log messages with a timestamp header.
      * Add some additional log messages for easier troubleshooting.
      * Enable debug logging for AWS CLI and AWS Encryption CLI invocations.

--- saltstack/base/salt/userdata-secrets/bin/cdp-retrieve-userdata-secrets.sh
+++ saltstack/base/salt/userdata-secrets/bin/cdp-retrieve-userdata-secrets.sh
@@ -4,6 +4,10 @@ set -e +x
 
 # DO NOT EXPORT environment variables in this file, because this file is called with `source` in `user-data-helper.sh`!
 
+log() {
+  echo "$(date +"%F-%T") $*"
+}
+
 # This function implements basically the same exponential backoff strategy as the aws cli in standard retry mode
 retry_until_non_placeholder_value() {
   local BASE_FACTOR=2
@@ -22,17 +26,17 @@ retry_until_non_placeholder_value() {
   set -e
   while [[ "$SECRET_STRING" == "PLACEHOLDER" || "$exit_code" == "$ACCESS_DENIED_EXIT_CODE" ]]; do
     if (( attempt > MAX_ATTEMPT )); then
-      echo "Maximum number of retry attempts reached while waiting for userdata secret to update! Total wait time was $total_wait_time seconds. Exiting..."
+      log "Maximum number of retry attempts reached while waiting for userdata secret to update! Total wait time was $total_wait_time seconds. Exiting..."
       exit 1
     fi
     if [[ "$exit_code" == "$ACCESS_DENIED_EXIT_CODE" ]]; then
       non_transient_aws_service_error_count=$(( non_transient_aws_service_error_count + 1 ))
-      echo "Getting the userdata secret failed with a non-transient error. See the error returned by aws-cli above! "
+      log "Getting the userdata secret failed with a non-transient error. See the error returned by aws-cli above! "
     else
-      echo "Userdata secret still has PLACEHOLDER value."
+      log "Userdata secret still has PLACEHOLDER value."
     fi
 
-    echo "Waiting $timeout seconds until the next attempt to retrieve the secret."
+    log "Waiting $timeout seconds until the next attempt to retrieve the secret."
     sleep $timeout
 
     total_wait_time=$(( total_wait_time + timeout ))
@@ -44,8 +48,8 @@ retry_until_non_placeholder_value() {
     exit_code=$?
     set -e
   done
-  echo "Total number of non-transient AWS errors: $non_transient_aws_service_error_count."
-  echo "Userdata secret's value was retrieved after waiting $total_wait_time seconds."
+  log "Total number of non-transient AWS errors: $non_transient_aws_service_error_count."
+  log "Userdata secret's value was retrieved after waiting $total_wait_time seconds."
 }
 
 # Helper function for passing aws-cli params related to the built-in retry mechanism, and param to instruct aws-cli to use FIPS endpoints
@@ -62,32 +66,36 @@ aws_with_cli_params() {
 # Intermittent issues like throttling of AWS APIs are handled by the built in retry mechanism of the aws cli.
 # Waiting for CB to update the secret's value to the actual secrets is handled by the `retry_until_non_placeholder_value` function.
 main() {
+  log "$(basename $0) Start"
+
   if [[ -z "$USERDATA_SECRET_ID" ]]; then
-    echo "USERDATA_SECRET_ID is empty. Cannot retrieve userdata secrets!"
+    log "USERDATA_SECRET_ID is empty. Cannot retrieve userdata secrets!"
     exit 1
   fi
 
   if [[ "$CLOUD_PLATFORM" == "AWS" ]]; then
-    echo "Retrieving userdata secrets..."
+    log "Retrieving userdata secrets..."
     retry_until_non_placeholder_value \
     aws_with_cli_params \
-    aws secretsmanager get-secret-value \
+    aws --debug secretsmanager get-secret-value \
       --output text \
       --query SecretString \
       --secret-id "$USERDATA_SECRET_ID"
     eval "$SECRET_STRING"
-    echo "Successfully retrieved userdata secrets!"
+    log "Successfully retrieved userdata secrets!"
 
-    echo "Deleting secret associated with this instance..."
+    log "Deleting secret associated with this instance..."
     aws_with_cli_params \
-    aws secretsmanager delete-secret \
+    aws --debug secretsmanager delete-secret \
       --force-delete-without-recovery \
       --secret-id "$USERDATA_SECRET_ID"
-    echo "Successfully deleted secret associated with this instance!"
+    log "Successfully deleted secret associated with this instance!"
   else
-    echo "Userdata secret retrieval not implemented for CLOUD_PLATFORM $CLOUD_PLATFORM."
+    log "Userdata secret retrieval not implemented for CLOUD_PLATFORM $CLOUD_PLATFORM."
     exit 2
   fi
+
+  log "$(basename $0) Finish"
 }
 
 main
