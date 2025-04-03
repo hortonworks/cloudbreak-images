0af5bf5c4c0758170bdf62f76bddce7082274c98
Gabor Solya <gsolya@cloudera.com>
2025-04-03 13:30:47 +0200

    CB-27058 Burn SaltStack 3006.9 onto images

--- scripts/get-salt-version.sh
+++ scripts/get-salt-version.sh
@@ -13,16 +13,16 @@ if [[ $BASE_NAME == "cb" ]]; then
     COMP_RESULT=$?
     # Stack version < 7.2.6 - Lowered this from 7.2.16, because we'll need Python 3.8 on older images too
     if [[ $COMP_RESULT == 2 ]]; then
-      SALT_VERSION=3000.8
+      SALT_VERSION=3006.9
     fi
     compare_version $STACK_VERSION 7.2.18
     COMP_RESULT=$?
     # Stack version >= 7.2.18
     if [[ $COMP_RESULT -lt 2 ]]; then
-      SALT_VERSION=3001.8 # reverted from 3006.5 until CB-21080	is fixed
+      SALT_VERSION=3006.9
     fi
   else # base image
-    SALT_VERSION=3001.8 # reverted from 3006.5 until CB-21080 is fixed
+    SALT_VERSION=3006.9
   fi
 fi
 
