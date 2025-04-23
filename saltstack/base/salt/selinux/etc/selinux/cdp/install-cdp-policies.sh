ca3affcd2953d9880f16d87fc08b2ca88f43da9a
Szabolcs Horvath <szaby0927@gmail.com>
2025-04-23 14:19:48 +0200

    CB-1: Salt as confined SELinux service

--- saltstack/base/salt/selinux/etc/selinux/cdp/install-cdp-policies.sh
+++ saltstack/base/salt/selinux/etc/selinux/cdp/install-cdp-policies.sh
@@ -3,9 +3,10 @@
 set -ex -o pipefail
 
 SELINUX_CDP_DIR=/etc/selinux/cdp
+LOG_FILE=/var/log/selinux/install-cdp-policies.log
 
 log() {
-  echo "$(date +"%F-%T") $*"
+  echo "$(date +"%F-%T") $*" >> "$LOG_FILE"
 }
 
 install_policy() {
@@ -16,10 +17,11 @@ install_policy() {
     log "Installing CDP SELinux policy '$policy_name' from $dir_abs_path"
     make -f /usr/share/selinux/devel/Makefile "$policy_name.pp" -C "$dir_abs_path"
     log "Compiled CDP SELinux policy '$policy_name'"
-    semodule -i "$dir_abs_path/$policy_name.pp"
+    semodule -i "$dir_abs_path/$policy_name.pp" -v
     log "Installed CDP SELinux policy '$policy_name'"
   else
     log "CDP SELinux policy '$policy_name' already installed. Skipping installation."
+    log "To reinstall, please remove the existing policy first: 'semodule -r $policy_name'"
   fi
 }
 
@@ -34,7 +36,13 @@ apply_file_contexts() {
     local path
     for path in "${paths[@]}"; do
       log "Applying file contexts to path '$path'"
-      restorecon -R -v -i "$path"
+      if [[ "$path" == "/var/log/salt" ]]; then
+        log "$(ls -laZ /var/log/salt)"
+      fi
+      restorecon -RvFi "$path"
+      if [[ "$path" == "/var/log/salt" ]]; then
+        log "$(ls -laZ /var/log/salt)"
+      fi
     done
     log "Applied file contexts for CDP SELinux policy '$policy_name'"
   else
@@ -68,7 +76,9 @@ apply_port_contexts() {
 }
 
 main() {
-  CDP_POLICY_COMMON=common
+  log "The security context of this script's process is '$(id -Z)'"
+
+  local CDP_POLICY_COMMON=common
   # Collect the directories containing CDP SELinux policy files
   mapfile -t CDP_POLICY_DIRS < <(find "$SELINUX_CDP_DIR" -mindepth 1 -maxdepth 1 -type d \! -name "$CDP_POLICY_COMMON" -exec basename {} \;)
   log "Found CDP SELinux policy directories: $CDP_POLICY_COMMON ${CDP_POLICY_DIRS[*]}"
