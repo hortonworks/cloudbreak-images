f6b83d4bd2f0369cbc8b40a084e0ac34bc499f09
Laszlo Merczel <laszlo.merczel@cloudera.com>
2025-05-07 09:38:56 +0200

    CB-29391: Updated freeipa-health-agent for ARM64 builds

--- scripts/packer.sh
+++ scripts/packer.sh
@@ -99,7 +99,7 @@ packer_in_container() {
   if ! [[ $FREEIPA_HEALTH_AGENT_RPM_URL =~ ^http.*rpm$ ]]; then
     if [[ "$OS" == "redhat8" ]]; then
       if [[ "$ARCHITECTURE" == "arm64" ]]; then
-        export FREEIPA_HEALTH_AGENT_RPM_URL="https://cloudera-build-us-west-1.vpc.cloudera.com/s3/build/65542775/thunderhead/1.x/redhat8/yum/freeipa-health-agent-3-SNAPSHOT.aarch64.rpm"
+        export FREEIPA_HEALTH_AGENT_RPM_URL="https://cloudera-build-us-west-1.vpc.cloudera.com/s3/build/66300108/thunderhead/1.x/redhat8/yum/freeipa-health-agent-3-SNAPSHOT.aarch64.rpm"
       else
         export FREEIPA_HEALTH_AGENT_RPM_URL="https://archive.cloudera.com/cdp-freeipa-artifacts/freeipa-health-agent-2.1.0.2-b2228.x86_64.rpm"
       fi
