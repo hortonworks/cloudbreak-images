d7d982e4d21af479156c8712ffeda7330f949b22
Kristof Kubina <kkubina@cloudera.com>
2025-05-09 14:01:07 +0200

    CB-29390 Avoid blank lines in hosts file

--- saltstack/base/salt/hostname/usr/local/bin/cleanup-hosts.sh
+++ saltstack/base/salt/hostname/usr/local/bin/cleanup-hosts.sh
@@ -3,7 +3,7 @@
 # Backup original file
 cp /etc/hosts /etc/hosts.bak
 
-# Remove lines marked as "Added by Google"
-grep -v '# Added by Google' /etc/hosts.bak > /etc/hosts
+# Remove lines marked as "Added by Google" and avoid blank lines
+grep -v '# Added by Google' /etc/hosts.bak | sed '/^$/d' > /etc/hosts
 
 echo "Removed Google-added lines from /etc/hosts. Backup saved as /etc/hosts.bak."
