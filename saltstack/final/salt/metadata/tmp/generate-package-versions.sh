31785799995d0b1f8bd8843656efe77bfbf31c4f
Laszlo Merczel <laszlo.merczel@cloudera.com>
2025-10-27 23:37:28 +0100

    CB-30959: Made package-versions generation more robust

--- saltstack/final/salt/metadata/tmp/generate-package-versions.sh
+++ saltstack/final/salt/metadata/tmp/generate-package-versions.sh
@@ -162,6 +162,4 @@ if [[ "$CLOUD_PROVIDER" == "AWS"* ]]; then
 	cat /tmp/package-versions.json | jq '. + {"imds": "v2"}' > /tmp/package-versions.json.tmp && mv /tmp/package-versions.json.tmp /tmp/package-versions.json
 fi
 
-chmod 644 /tmp/package-versions.json
-
 exit 0
