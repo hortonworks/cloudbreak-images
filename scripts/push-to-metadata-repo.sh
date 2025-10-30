d2f7b62ab73260ad3698c45febfa43b2fbdb1679
Kristof Kubina <kkubina@cloudera.com>
2025-10-30 18:24:12 +0100

    CB-31009 Fix order of git operations in push-to-metadata-repo.sh to make it more robust

--- scripts/push-to-metadata-repo.sh
+++ scripts/push-to-metadata-repo.sh
@@ -15,7 +15,12 @@ if [ "$CLOUD_PROVIDER" != "YARN" ]; then
   cp installed-delta-packages.csv "${GITHUB_REPO}/manifest/${UUID}-manifest.csv"
   cp installed-full-packages.csv "${GITHUB_REPO}/manifest/${UUID}-full-manifest.csv"
 fi
+
 pushd $GITHUB_REPO
+git add -A
+git commit -am "Upload new metadata files for ${UUID}"
+
 git pull --rebase origin master
-git add -A && git commit -am "Upload new metadata files for ${UUID}" && git push
+
+git push
 popd
