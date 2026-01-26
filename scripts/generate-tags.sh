0e755301810da3d89b55573ad5e2f4e50c58334c
Laszlo Merczel <laszlo.merczel@cloudera.com>
2026-01-26 14:53:47 +0100

    CB-31338 Add support for RHEL9 on AWS Gov

--- scripts/generate-tags.sh
+++ scripts/generate-tags.sh
@@ -8,6 +8,7 @@ fi
 
 if [[ "$OS" == "redhat8" || "$OS" == "redhat9" ]]; then
     FIPSMODE=enabled
+    fips-mode-setup --is-enabled
     fips-mode-setup --is-enabled || FIPSMODE=disabled
 
     TAGS=$(echo $TAGS | jq --arg fipsmode $FIPSMODE -r '. + {"fips-mode": $fipsmode}')
