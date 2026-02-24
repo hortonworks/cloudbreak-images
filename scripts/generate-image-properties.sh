821902e0d90c8b7709475411eb950b8eefb37479
Kristof Kubina <kkubina@cloudera.com>
2026-02-24 13:48:48 +0100

    CB-31904 Try long epoch in image name from generate-image-properties.sh

--- scripts/generate-image-properties.sh
+++ scripts/generate-image-properties.sh
@@ -6,7 +6,7 @@
 # - BUILD_ID
 
 STACK_VERSION_SHORT=${STACK_TYPE}-$(echo ${STACK_VERSION} | tr -d . | cut -c1-4 )
-IMAGE_NAME=${BASE_NAME}-$(echo ${STACK_VERSION_SHORT} | tr '[:upper:]' '[:lower:]')-$(date +%s)
+IMAGE_NAME=${BASE_NAME}-$(echo ${STACK_VERSION_SHORT} | tr '[:upper:]' '[:lower:]')-$(date +%s%3N)
 
 echo IMAGE_NAME=${IMAGE_NAME} >> image.properties
 echo METADATA_FILENAME_POSTFIX=${BUILD_ID} >> image.properties
