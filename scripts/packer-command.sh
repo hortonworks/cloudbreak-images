e67e128601e858adb9f91a9707d50d70f761d955
pkergyik <pkergyik@cloudera.com>
2026-03-02 17:31:10 +0100

    CB-31564 creds check for testing

--- scripts/packer-command.sh
+++ scripts/packer-command.sh
@@ -7,6 +7,10 @@ function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4
 
 if [[ -n "$PACKER_GITHUB_API_TOKEN" ]]; then
   echo "PACKER_GITHUB_API_TOKEN is set."
+  # Retrieve the cat: it should fail on auth error.
+  # Some documentation also notes that sometimes Bearer or token prefix before the actual token is required depending on the token's type.
+  # For testing purposes let's start with this.
+  curl -L -H "Authorization: $PACKER_GITHUB_API_TOKEN" https://api.github.com/octocat
 fi
 
 if [ "$(version "$PACKER_VERSION")" -ge "$(version "1.10.0")" ]; then
