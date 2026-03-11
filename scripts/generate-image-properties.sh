1260843904df415b7d6f2a897b40409c1e1ff97a
Kristof Kubina <kkubina@cloudera.com>
2026-03-11 09:20:32 +0100

    CB-31904 New naming convention for IMAGE_NAME: freeipa- , cdp- , base- and long epoch

--- scripts/generate-image-properties.sh
+++ scripts/generate-image-properties.sh
@@ -5,13 +5,23 @@
 # - STACK_VERSION
 # - BUILD_ID
 
-if [[ "$BASE_NAME" == "freeipa" ]]; then    # Omit STACK_VERSION_SHORT for freeipa
-    IMAGE_NAME=${BASE_NAME}-$(date +%s%3N)
+
+LONG_EPOCH=$(date +%s%3N)
+
+if [[ "$BASE_NAME" == "freeipa" ]]; then
+    # Omit middle section for freeipa
+    IMAGE_NAME="freeipa-${LONG_EPOCH}"
 else
-    # Original logic for all other base names
-    STACK_VERSION_SHORT=${STACK_TYPE}-$(echo ${STACK_VERSION} | tr -d . | cut -c1-4 )
-    IMAGE_NAME=${BASE_NAME}-$(echo ${STACK_VERSION_SHORT} | tr '[:upper:]' '[:lower:]')-$(date +%s%3N)
+    if [[ -z "$STACK_VERSION" ]]; then
+        # STACK_VERSION is empty: do NOT include middle section
+        IMAGE_NAME="base-${LONG_EPOCH}"
+    else
+        # STACK_VERSION exists: include the middle section
+        # Note: Added tr to lowercase the STACK_TYPE as well for consistency
+        STACK_VERSION_SHORT=$(echo "${STACK_VERSION}" | tr -d . | cut -c1-4 | tr '[:upper:]' '[:lower:]')
+        IMAGE_NAME="cdp-${STACK_VERSION_SHORT}-${LONG_EPOCH}"
+    fi
 fi
 
-echo IMAGE_NAME=${IMAGE_NAME} >> image.properties
-echo METADATA_FILENAME_POSTFIX=${BUILD_ID} >> image.properties
+echo "IMAGE_NAME=${IMAGE_NAME}" >> image.properties
+echo "METADATA_FILENAME_POSTFIX=${BUILD_ID}" >> image.properties
