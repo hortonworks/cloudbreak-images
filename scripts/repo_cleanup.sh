b8821aeef895cfc2a50771c580e283b3aa8ceadf
Laszlo Merczel <laszlo.merczel@cloudera.com>
2026-03-12 20:08:09 +0100

    CB-32234 Fix PostgreSQL 17 setup for AWS Gov

--- scripts/repo_cleanup.sh
+++ scripts/repo_cleanup.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -xe
 
-if [[ "$CLOUD_PROVIDER" != "AWS_GOV" ]] ; then
-    if [[ "${OS}" == "redhat8" || "${OS}" == "redhat9" ]] ; then
-        if [[ "${IMAGE_BURNING_TYPE}" == "base" ]] ; then
-            RHEL_VERSION=$(cat /etc/redhat-release | grep -oP "[0-9\.]*")
-            RHEL_VERSION=${RHEL_VERSION/.0/}
-            REPO_FILE=rhel${RHEL_VERSION}_cldr_mirrors.repo
-            find /etc/yum.repos.d -type f ! -name $REPO_FILE -delete
-        else
-            find /etc/yum.repos.d -type f -delete
-        fi
+if [[ "${OS}" == "redhat8" || "${OS}" == "redhat9" ]] ; then
+    if [[ "${IMAGE_BURNING_TYPE}" == "base" ]] ; then
+        RHEL_VERSION=$(cat /etc/redhat-release | grep -oP "[0-9\.]*")
+        RHEL_VERSION=${RHEL_VERSION/.0/}
+        REPO_FILE=rhel${RHEL_VERSION}_cldr_mirrors.repo
+        find /etc/yum.repos.d -type f ! -name $REPO_FILE -delete
+    else
+        find /etc/yum.repos.d -type f -delete
     fi
 fi
--- scripts/salt-install.sh
+++ scripts/salt-install.sh
@@ -39,18 +39,11 @@ function install_salt_with_pip3() {
 }
 
 function install_with_yum() {
-  update_yum_repos
 
   # This is no longer needed for RHEL 9 - https://access.redhat.com/solutions/6973382
   if [ "${OS_TYPE}" == "redhat8" ] ; then
     yum install -y redhat-lsb-core
-  else
-    yum update -y python
   fi
-
-  yum install -y yum-utils yum-plugin-versionlock
-  yum clean metadata
-  enable_epel_repository
   yum groupinstall -y 'Development Tools'
   
   install_python_pip
@@ -66,50 +59,6 @@ function install_with_yum() {
   install_salt_with_pip3
   
   create_temp_minion_config
-}
-
-function update_yum_repos() {
-  if [[ "${OS}" == "redhat8" || "${OS}" == "redhat9" ]] ; then
-    # Remove RHEL official repos and use the internal mirror in case of RHEL 8/9
-    if [[ "${CLOUD_PROVIDER}" != "AWS_GOV" ]]; then
-      # Internal repo is not yet available for AWS_GOV images
-      RHEL_VERSION=$(cat /etc/redhat-release | grep -oP "[0-9\.]*")
-      RHEL_VERSION=${RHEL_VERSION/.0/}
-
-      # CB-30236: We need this override, because we only have a 9.5 base image for Azure
-      if [[ "${CLOUD_PROVIDER}" == "Azure" && "${RHEL_VERSION}" == "9.5" ]]; then
-        RHEL_VERSION="9.6"
-      fi
-
-      RHEL_VERSION_MAJOR=${RHEL_VERSION:0:1}
-      REPO_FILE=rhel${RHEL_VERSION}_cldr_mirrors.repo
-      if [ "${CLOUD_PROVIDER}" != "YARN" ]; then
-        rm /etc/yum.repos.d/*.repo -f
-      fi
-      curl https://mirror.eng.cloudera.com/repos/rhel/server/${RHEL_VERSION_MAJOR}/${RHEL_VERSION}/${REPO_FILE} --fail > /etc/yum.repos.d/${REPO_FILE}
-    fi
-  else
-    # Workaround based on the official documentation: https://cloud.google.com/compute/docs/troubleshooting/known-issues#known_issues_for_linux_vm_instances
-    if [ "${CLOUD_PROVIDER}" == "GCP" ]; then
-      sudo sed -i 's/repo_gpgcheck=1/repo_gpgcheck=0/g' /etc/yum.repos.d/google-cloud.repo
-    fi
-
-    # Replace repos on the image that are no longer working
-    sudo rm -rf /etc/yum.repos.d/CentOS*.repo
-    cp /tmp/repos/RPM-GPG-KEY-CentOS-SIG-SCLo /etc/pki/rpm-gpg/
-    cp /tmp/repos/centos-vault.repo /etc/yum.repos.d/
-  fi
-}
-
-function enable_epel_repository() {
-  if [ "${OS}" == "redhat9" ] ; then
-    dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-  elif [ "${OS}" == "redhat8" ] ; then
-    dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-  elif [ "${OS}" == "centos7" ] ; then
-    cp /tmp/repos/RPM-GPG-KEY-CentOS-EPEL /etc/pki/rpm-gpg/
-    cp /tmp/repos/centos-epel.repo /etc/yum.repos.d/
-  fi
 }
 
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
