fd2e5a99754d8efbf906f8492e8dc98880c82148
pkergyik <pkergyik@cloudera.com>
2026-03-27 18:11:55 +0100

    CB-28076 packer resource group deleation workaround attempt

--- scripts/generate-image-properties.sh
+++ scripts/generate-image-properties.sh
@@ -10,16 +10,16 @@ LONG_EPOCH=$(date +%s%3N)
 
 if [[ "$BASE_NAME" == "freeipa" ]]; then
     # Omit middle section for freeipa
-    IMAGE_NAME="freeipa-${LONG_EPOCH}"
+    IMAGE_NAME="freeipa_${LONG_EPOCH}"
 else
     if [[ -z "$STACK_VERSION" ]]; then
         # STACK_VERSION is empty: do NOT include middle section
-        IMAGE_NAME="base-${LONG_EPOCH}"
+        IMAGE_NAME="base_${LONG_EPOCH}"
     else
         # STACK_VERSION exists: include the middle section
         # Note: Added tr to lowercase the STACK_TYPE as well for consistency
         STACK_VERSION_SHORT=$(echo "${STACK_VERSION}" | tr -d . | cut -c1-4 | tr '[:upper:]' '[:lower:]')
-        IMAGE_NAME="cdp-${STACK_VERSION_SHORT}-${LONG_EPOCH}"
+        IMAGE_NAME="cdp_${STACK_VERSION_SHORT}-${LONG_EPOCH}"
     fi
 fi
 
